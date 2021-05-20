eef3ff4d5200e931ffaa225f2aed23b9b1d17b5a
Renan Ponick <renan.ponic@ngi.com.br>
2021-05-20 01:16:51 -0300

    Push Heroku

--- src/database.ts
+++ src/database.ts
@@ -1,85 +1,8 @@
-import { snakeCase } from 'change-case'
-import { dropLast } from 'ramda'
 import { Container } from 'typedi'
 import * as TypeORM from 'typeorm'
 
 import config from './config'
 import log from './logger'
-
-class AppNamingStrategy extends TypeORM.DefaultNamingStrategy {
-
-    private readonly maxIndexNameLength = 63
-
-    private getTableName(tableOrName: TypeORM.Table | string) {
-        return tableOrName instanceof TypeORM.Table
-            ? tableOrName.name
-            : tableOrName
-    }
-
-    private joinColumns(columnNames: string[], suffix = 'key') {
-        return [...columnNames, suffix]
-            .map(name => snakeCase(name, undefined))
-            .join('_')
-    }
-
-    joinColumnName(relationName: string) {
-        return relationName
-    }
-
-    joinTableColumnName(tableName: string) {
-        return tableName
-    }
-
-    foreignKeyName(tableOrName: TypeORM.Table | string, columnNames: string[]) {
-        const columns = this.joinColumns(columnNames, 'fkey')
-
-        return `${this.getTableName(tableOrName)}_${columns}`
-    }
-
-    columnName(
-        propertyName: string,
-        customName: string | undefined,
-        prefixes: string[]
-    ) {
-        return [...prefixes, customName || propertyName]
-            .map(name => snakeCase(name, undefined))
-            .join('_')
-    }
-
-    primaryKeyName(tableOrName: TypeORM.Table | string) {
-        return `${this.getTableName(tableOrName)}_pkey`
-    }
-
-    uniqueConstraintName(
-        tableOrName: TypeORM.Table | string,
-        columnNames: string[]
-    ) {
-        const tableName = this.getTableName(tableOrName)
-        let columns = columnNames
-        let name = `${tableName}_${this.joinColumns(columns, 'idx')}`
-
-        while (name.length > this.maxIndexNameLength) {
-            columns = dropLast(1, columns)
-            name = `${tableName}_${this.joinColumns(columns, 'idx')}`
-        }
-
-        return name
-    }
-
-    indexName(tableOrName: TypeORM.Table | string, columnNames: string[]) {
-        const tableName = this.getTableName(tableOrName)
-        let columns = columnNames
-        let name = `${tableName}_${this.joinColumns(columns, 'key')}`
-
-        while (name.length > this.maxIndexNameLength) {
-            columns = dropLast(1, columns)
-            name = `${tableName}_${this.joinColumns(columns, 'key')}`
-        }
-
-        return name
-    }
-
-}
 
 const poolSize = 200
 const connectionTimeoutMillis = 300000
@@ -93,7 +16,6 @@ TypeORM.createConnection({
     entities: [
         `${__dirname}/models/*`
     ],
-    namingStrategy: new AppNamingStrategy(),
     logging: logs.db ? 'all' : false,
     ssl: true,
     synchronize: true,
